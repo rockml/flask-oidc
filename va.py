b8735a5679abb8f9fec04c3d3f4012403270cdbd
Wendell Ocasio <wendell.ocasio@gmail.com>
2019-04-20 11:50:24 -0400

    va snippets

--- va.py
+++ va.py
@@ -1,6 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+import json
+import requests as req
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+tokenfile = open("accesstoken.txt", "r")
+token = tokenfile.read()
+tokenfile.close()
+
+headers = {"Authorization": "Bearer {}".format(token)}
+
+res = req.get("https://dev-api.va.gov/oauth2/userinfo", headers=headers)
+print(res)
+print(res.json())
+
+res = req.get("https://api.vets.gov/services/argonaut/v0/Patient/1012829910V765228", headers=headers)
+print(res)
+res.json()
+
+res = req.get("https://api.vets.gov/services/veteran_verification/v0/status", headers=headers)
+print(res)
+res.json()
+
